07b53d0c6a802d7578ba1123dcfc85b85d8e63a4
Radha Kotra <kradha6113@gmail.com>
2025-03-03 14:43:29 -0500

    Add tables users and strava_credentials

--- db/populatedb.js
+++ db/populatedb.js
@@ -5,16 +5,34 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const SQL = `
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255)
+);
+
+CREATE TABLE strava_credentials (
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    client_id VARCHAR(255),
+    client_secret VARCHAR(255),
+    access_token VARCHAR(255),
+    refresh_token VARCHAR(255),
+    expires_at TIMESTAMP,
+    PRIMARY KEY (user_id)
+);
+
 CREATE TABLE IF NOT EXISTS planned_activities (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     title VARCHAR(255),
     date DATE,
     type VARCHAR(50),
     distance INTEGER,
     duration INTEGER,
     route VARCHAR(255),
-    shoes VARCHAR(255),
-    strava_id BIGINT
+    shoes VARCHAR(255)
 );
 `;
 

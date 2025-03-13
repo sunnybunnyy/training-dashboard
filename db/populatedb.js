12e6fa69c8dd1b2c41146d6dc306ea97723a559c
Radha Kotra <kradha6113@gmail.com>
2025-03-13 14:46:02 -0400

    Create a table for training plans and add column plan_id to planned_activities table

--- db/populatedb.js
+++ db/populatedb.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const SQL = `
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE users (
     last_name VARCHAR(255)
 );
 
-CREATE TABLE strava_credentials (
+CREATE TABLE IF NOT EXISTS strava_credentials (
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     client_id VARCHAR(255),
     client_secret VARCHAR(255),
@@ -26,6 +26,7 @@ CREATE TABLE strava_credentials (
 CREATE TABLE IF NOT EXISTS planned_activities (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    plan_id INTEGER REFERENCES training_plans(id) ON DELETE CASCADE,
     title VARCHAR(255),
     date DATE,
     type VARCHAR(50),
@@ -33,6 +34,14 @@ CREATE TABLE IF NOT EXISTS planned_activities (
     duration INTEGER,
     route VARCHAR(255),
     shoes VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS training_plans (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL,
+    color VARCHAR(20) NOT NULL,
+    description TEXT
 );
 `;
 

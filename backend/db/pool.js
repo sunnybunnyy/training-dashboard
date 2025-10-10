37b6976ab502fa7758d4b18ef59158c39a6d4bd3
Radha Kotra <kradha6113@gmail.com>
2025-10-09 20:52:32 -0400

    Add __dirname and __filename

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import pkg from 'pg';
 const { Pool } = pkg;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 const isProduction = process.env.NODE_ENV === 'production';

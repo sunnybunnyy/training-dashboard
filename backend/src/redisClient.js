c80b1df2f2beb50b0f2bf62ace5d9934cce91a39
Radha Kotra <kradha6113@gmail.com>
2025-10-10 21:36:55 -0400

    Use public endpint for Redis

--- backend/src/redisClient.js
+++ backend/src/redisClient.js
@@ -2,7 +2,12 @@ import { createClient } from 'redis';
 
 
 const client = createClient({
-    url: process.env.REDIS_URL
+    username: 'default',
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
 });
 
 client.on('error', err => console.log('Redis Client Error', err));

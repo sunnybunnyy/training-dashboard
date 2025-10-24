016616b9c1e1e5897e2d169a72f1d833f4a6350e
Radha Kotra <kradha6113@gmail.com>
2025-10-23 21:02:02 -0400

    Edit strava_activities table to have more fields

--- backend/db/populatedb.js
+++ backend/db/populatedb.js
@@ -50,6 +50,12 @@ CREATE TABLE IF NOT EXISTS strava_activities (
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     strava_id BIGINT NOT NULL,
     plan_id INTEGER REFERENCES training_plans(id) ON DELETE SET NULL,
+    date DATE,
+    distance INTEGER,
+    duration INTEGER,
+    avg_speed FLOAT,
+    avg_hr FLOAT,
+    type VARCHAR(50),
     UNIQUE(user_id, strava_id)
 );
 
